c4246d9c26ff0c889ba383bd136938021431a7f4
Mohamed Jubair <amzubair@live.com>
2026-02-03 13:58:36 +0530

    Create a hello world next-js application Fixes #2

---
+++ apps/hello-world/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <span className="text-4xl">Hello World!</span>
+    </div>
+  );
+}
