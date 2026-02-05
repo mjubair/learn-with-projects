1f2b81fc3aa49cd6e8846128a17ae0dc4e27a881
Mohamed Jubair <amzubair@live.com>
2026-02-05 17:54:25 +0530

    Add basic structure to profile card Fixes #16

--- apps/great-frontend-projects/profile-card/src/App.tsx
+++ apps/great-frontend-projects/profile-card/src/App.tsx
@@ -1,28 +1,39 @@
 import Card from './components/card'
 import Avatar from './components/avatar.tsx'
 import avatar from './assets/profile.png'
+import { RiGithubFill, RiLinkedinFill, RiInstagramFill, RiXboxFill } from "@remixicon/react";
 
 function App() {
 
   return (
-    <div className="flex justify-center items-center">
-      <Card>
-        <Avatar imageUrl={avatar} />
-        <h2 className="text-2xl font-bold mt-4">Sarah Dole</h2>
-        <p className="text-gray-600 mt-2">Front End Engineer @ Microsoft</p>
-        <p className="text-gray-600 mt-2">I turn coffee into bugs which are fixed by someone else. Certified Stack Overflow and ChatGPT developer.</p>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Contact me</button>
-        
-      </Card>
+			<div className="flex justify-center items-center">
+				<Card className="flex flex-col items-center">
+					<Avatar imageUrl={avatar} />
+					<h2 className="text-2xl font-bold mt-4">Sarah Dole</h2>
+					<p className="text-gray-600 mt-2">Front End Engineer @ Microsoft</p>
+					<p className="text-gray-600 mt-2 text-center">
+						I turn coffee into bugs which are fixed by someone else. Certified
+						Stack Overflow and ChatGPT developer.
+					</p>
+					<button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+						Contact me
+					</button>
+          <div className="flex gap-9">
+            <RiGithubFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+            <RiLinkedinFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+            <RiInstagramFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+            <RiXboxFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+          </div>
+				</Card>
 
-      {/* 
+				{/* 
       Sarah Dole
       Front End Engineer @ Microsoft
       I turn coffee into bugs which are fixed by someone else. Certified Stack Overflow and ChatGPT developer.
       Contact me
       */}
-    </div>
-  )
+			</div>
+		);
 }
 
 export default App
--- apps/great-frontend-projects/profile-card/src/components/card.tsx
+++ apps/great-frontend-projects/profile-card/src/components/card.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from 'react';
 
 interface CardProps {
   children: ReactNode;
+  className?: string;
 }
 
-const Card = ({children}: CardProps) => {
+const Card = ({children, className}: CardProps) => {
   return (
-    <div className="shadow-sm max-w-85 p-4 rounded-lg mt-50 bg-white">
+    <div className={`shadow-sm max-w-85 p-4 rounded-lg mt-50 bg-white ${className}`}>
       {children}
     </div>  
   );
