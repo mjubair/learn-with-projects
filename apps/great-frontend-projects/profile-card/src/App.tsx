e6d0e624f74f28e4fceb257f2c67ba5e9e29b1ee
Mohamed Jubair <amzubair@live.com>
2026-02-09 14:51:09 +0530

    Refactor profile card layout and adjust avatar size for better presentation

--- apps/great-frontend-projects/profile-card/src/App.tsx
+++ apps/great-frontend-projects/profile-card/src/App.tsx
@@ -9,21 +9,24 @@ function App() {
 			<div className="flex justify-center items-center">
 				<Card className="flex flex-col items-center">
 					<Avatar imageUrl={avatar} />
-					<h2 className="text-2xl font-bold mt-4">Sarah Dole</h2>
-					<p className="text-gray-600 mt-2">Front End Engineer @ Microsoft</p>
-					<p className="text-gray-600 mt-2 text-center">
+					<h2 className="text-[20px] font-bold mt-8">Sarah Dole</h2>
+					<p className="text-gray-600 mt-1">Front End Engineer @ Microsoft</p>
+					<p className="text-gray-600 mt-8 text-center">
 						I turn coffee into bugs which are fixed by someone else. Certified
 						Stack Overflow and ChatGPT developer.
 					</p>
-					<button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+					<button
+						type="button"
+						className="mt-10 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+					>
 						Contact me
 					</button>
-          <div className="flex gap-9">
-            <RiGithubFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
-            <RiLinkedinFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
-            <RiInstagramFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
-            <RiXboxFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
-          </div>
+					<div className="flex gap-9 mt-6">
+						<RiGithubFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+						<RiLinkedinFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+						<RiInstagramFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+						<RiXboxFill className="w-5 h-5 mt-4 text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
+					</div>
 				</Card>
 
 				{/* 
--- apps/great-frontend-projects/profile-card/src/components/avatar.tsx
+++ apps/great-frontend-projects/profile-card/src/components/avatar.tsx
@@ -4,8 +4,12 @@ interface AvatarProps {
 
 const Avatar = ({imageUrl}: AvatarProps) => {
     return (
-        <img src={imageUrl} alt="Avatar" className="w-24 h-24 rounded-full mx-auto" />
-    );
+					<img
+						src={imageUrl}
+						alt="Avatar"
+						className="w-16 h-16 rounded-full mx-auto"
+					/>
+				);
 }
 
 export default Avatar;
