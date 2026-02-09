34d1a8e3c43905ccd97563cfd4925318e6de830b
Mohamed Jubair <amzubair@live.com>
2026-02-09 16:40:11 +0530

    Adjust padding in Card component for improved layout consistency

--- apps/great-frontend-projects/profile-card/src/components/card.tsx
+++ apps/great-frontend-projects/profile-card/src/components/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const Card = ({children, className}: CardProps) => {
   return (
-    <div className={`shadow-sm max-w-85 p-4 rounded-lg mt-50 bg-white ${className}`}>
+    <div className={`shadow-sm max-w-85 py-6 px-4 rounded-lg mt-50 bg-white ${className}`}>
       {children}
     </div>  
   );
